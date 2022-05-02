b24af606cba8b65e097f67f85414cb0c858d6259
armensano <armen.sanoyan@mail.zenbit.tech>
2022-05-02 18:23:15 +0400

    cool app

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): object {
-    debugger;
-    return { message: 'nex try!' };
+    return { message: 'what the fuck!' };
   }
 }
