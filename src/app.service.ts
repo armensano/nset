93477cf95a30d73da4c8c131ed9bf37c3fa2353f
armensano <armen.sanoyan@mail.zenbit.tech>
2022-05-02 17:56:53 +0400

    try caching again

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): object {
     debugger;
-    return { message: 'cache node_modules!' };
+    return { message: 'another try!' };
   }
 }
