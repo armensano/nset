57deb52bcc5f5d2f68b1e6374d3a867c9395c9b1
armensano <armen.sanoyan@mail.zenbit.tech>
2022-05-02 18:15:00 +0400

    next try

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): object {
     debugger;
-    return { message: 'try until it works!' };
+    return { message: 'nex try!' };
   }
 }
